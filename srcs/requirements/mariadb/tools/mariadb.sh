58cc552b046c50a7cae938561439d0f853d92592
Arik Hativa <arikhativa@gmail.com>
2023-09-04 15:18:00 +0300

    ftp almot done

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,14 +5,12 @@
 # 	echo "Database already exists"
 # else
 
-
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then 
 	echo "Database already exists"
+	exec mysqld
+	exit 0
 else
-
-sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
-# sed -i 's/#bind-address/bind-address/g' /etc/my.cnf.d/mariadb-server.cnf
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /tmp/init.sql
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /tmp/init.sql
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -35,6 +35,12 @@ wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} 
 wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 wp user create --allow-root "${WP_USER}" "${WP_EMAIL}" --user_pass="${WP_PASSWORD}" --role=author
 
+wp config set --allow-root FS_METHOD ftpext
+wp config set --allow-root FTP_HOST $FTP_HOST
+wp config set --allow-root FTP_USER $FTP_USER
+wp config set --allow-root FTP_PASS $FTP_PASS
+wp config set --allow-root FTP_PORT 21
+
 fi
 
 exec "$@"
